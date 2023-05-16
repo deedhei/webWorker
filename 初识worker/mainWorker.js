a984a4731f13b07e32aaab82328103fc3175d5f6
朱兴 <3444982748@qq.com>
2023-05-16 13:28:05 +0800

    初识worker

---
+++ 初识worker/mainWorker.js
@@ -0,0 +1,15 @@
+const first = document.querySelector("#number1");
+const second = document.querySelector("#number2");
+
+const result = document.querySelector(".result");
+console.log("[Log] window.Worker-->", window.Worker);
+if (window.Worker) {
+  let worker = new Worker("work.js");
+  first.onchange = function (e) {
+    worker.postMessage([first.value, second.value]); // work.js文件用onmessage来接收线程发过去的事件
+    console.log("first", e);
+  };
+  second.onchange = function (e) {
+    console.log("second", e);
+  };
+}
